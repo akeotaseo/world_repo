799de974d0171d83a91fe03dcd5a6e8e7dade4c5
akeotaseo <44805256+akeotaseo@users.noreply.github.com>
2021-12-31 23:16:32 +0200

    Add files via upload

---
+++ Updater_Matrix/addons_repos.py
@@ -0,0 +1,8 @@
+
+ADDONS_REPOS = [('plugin.video.fmoviesto', 'repository.mbebe'),
+                ('plugin.video.subsmovies', 'repository.mbebe'),
+                ('plugin.video.cartoonsgr', 'repository.gkobu'),
+                ('repository.NarcacistWizard', 'repository.NarcacistWizard'),
+                ('plugin.video.Rising.Tides', 'repository.Rising.Tides'),
+                ('repository.newdiamond', 'repository.newdiamond'),
+                ('script.extendedinfo', 'repository.World')]
